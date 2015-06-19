d72a02d53d9c0e02976278ddbe3697ecf0f618dd
McCade Larsen 4H <mlarsen@uranium.cs.dixie.edu>
2015-06-19 12:49:32 -0600

    angular js added

---
+++ angularjs_day1/javascripts/application.js
@@ -0,0 +1,21 @@
+(function(){
+	angular.module('myApp', [])
+
+		.controller('applicationCtrl', function()
+		{
+			this.imagesNames = [
+				'cat.jpeg',
+				'catduck.jpeg',
+				'heghog.jpeg',
+				'mouse.jpeg',
+				'snow.jpeg'
+				
+
+
+			];
+
+
+		})
+
+
+}).call(this);
