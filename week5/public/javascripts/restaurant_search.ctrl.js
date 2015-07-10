559df5c3bbd892568c6a82fd594cd8401bdf7223
McCade Larsen 4H <mlarsen@uranium.cs.dixie.edu>
2015-07-10 12:53:31 -0600

    stuff

--- week5/public/javascripts/restaurant_search.ctrl.js
+++
@@ -1,8 +0,0 @@
-(function () {
-	angular.module("lunchApp")
-		.controller("RestaurantSearchCtrl",["Restaurant", "$rootScope", "$state", function (Restaurant, $rootScope, $state){
-			this.searchRestaurant = function(){
-				console.log("DOES THIS DO ANYTHING?");
-			};
-		}]);
-})();
